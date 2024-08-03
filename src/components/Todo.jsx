44743e46b8acafd3b6930b41b2137d3cbfab83f5
anushka1610-yadav <anushkaydv1610@gmail.com>
2024-08-03 22:29:17 +0530

    added the basic stucture

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -3,7 +3,16 @@ import TodoItems from "./TodoItems";
 
 const Todo = () => {
     return (
-        <TodoItems />
+        <div className="todo">
+            <div className="todo-header">To Do List</div>
+            <div className="todo-add">
+                <input type="text" placeholder='Add Your Task' className="todo-input" />
+                <div className="todo-add-btn">Add</div>
+            </div>
+            <div className="todo-list">
+                
+            </div>
+        </div>
     )
 }
 
